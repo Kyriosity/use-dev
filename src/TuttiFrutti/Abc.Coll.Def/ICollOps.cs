c1ee77ef930799ea413f74f8eb6a2d20df7193ec
Kiryl Drougakov <kiryl_d@gmx.net>
2023-04-25 20:43:07 +0200

    Tutti-Frutti > Colls > start definitions

---
+++ src/TuttiFrutti/Abc.Coll.Def/ICollOps.cs
@@ -0,0 +1,14 @@
+﻿namespace Abc.Coll.Def;
+
+public interface ICollOps<T>
+{
+    void Add(T item);
+    void Add(IEnumerable<T> items);
+    void Remove(int at = -1, int count = 1);
+}
+
+public interface IItemsAccu<T> : ICollOps<T> //, IAdjustedInput<T>
+{
+    T this[int index] { get; }
+    IReadOnlyCollection<T> Items { get; }
+}
