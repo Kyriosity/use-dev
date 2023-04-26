351bebbc6e680ecb8aeb5bd33061f051f2ddde1e
Kiryl Drougakov <kiryl_d@gmx.net>
2023-04-26 14:38:14 +0200

    Tutti-Frutti > Reversible > Definitions

---
+++ src/TuttiFrutti/Rvrs.Def/IReversible.cs
@@ -0,0 +1,39 @@
+﻿namespace Rvrs.Def;
+
+public interface IReversible { }
+
+public interface IBackward : IReversible
+{
+    void Undo();
+    int StepsUndo();
+}
+
+public interface IBackAndForth : IBackward
+{
+    void Redo();
+    int StepsRedo();
+}
+
+public interface IBackwardTill : IReversible
+{
+    int Undo(DateTime till);
+    int StepsUndo(DateTime till);
+}
+
+public interface IBackAndForthTill : IBackwardTill
+{
+    int Redo(DateTime till);
+    int StepsRedo(DateTime till);
+}
+
+public interface IBackwardProvided<T> : IReversible
+{
+    int Undo(Predicate<T> till);
+    int StepsUndo(Predicate<T> till);
+}
+
+public interface IBackAndForthProvided<T> : IBackwardProvided<T>
+{
+    int Redo(Predicate<T> till);
+    int StepsRedo(Predicate<T> till);
+}
