81d2f613cf0a7ec9a6a557880a0931ca7bd6c556
Kiryl Drougakov <kiryl_d@gmx.net>
2023-05-23 18:07:11 +0200

    abc>err: exception shortcut [added]

---
+++ src/TuttiFrutti/Abc/Err/Sys/NotImplemented.cs
@@ -0,0 +1,7 @@
+﻿namespace Abc.Err.Sys;
+public static class NotImplemented
+{
+    public static dynamic Throw() => throw new NotImplementedException();
+    public static dynamic Throw(string? message) => throw new NotImplementedException(message);
+    public static dynamic Throw(string? message, Exception? inner) => throw new NotImplementedException(message, inner);
+}
