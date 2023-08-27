39a59ca2e1cbff41716996708399fd1d878312f5
Kiryl Drougakov <kiryl_d@gmx.net>
2023-08-27 19:47:15 +0200

    WPF Clay:  1) project created, 2) BoolVsViz added

---
+++ src/TuttiFrutti/Win.Clay/Converters/BoolVsVizConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Win.Clay.Converters;
+
+[ValueConversion(typeof(Visibility), typeof(bool))]
+public class BoolVsVizConverter : IValueConverter
+{
+    public Visibility True { get; set; } = Visibility.Visible;
+    public Visibility False { get; set; } = Visibility.Hidden;
+    public bool? DefaultBack { get; set; }
+
+    public virtual object? Convert(object value, Type _, object __, CultureInfo ___) {
+        if (value is null)
+            return null;
+
+        if (value is not bool visible)
+            return DependencyProperty.UnsetValue;
+
+        return visible ? True : False;
+    }
+
+    public virtual object ConvertBack(object value, Type _, object __, CultureInfo ___) {
+        if (value is not Visibility viz)
+            return DependencyProperty.UnsetValue;
+
+        if (TryMatch(viz, out var visible))
+            return visible;
+
+        return DefaultBack ?? DependencyProperty.UnsetValue;
+    }
+
+    private bool TryMatch(Visibility? viz, out bool res) {
+        res = false;
+
+        if (viz == True)
+            res = true;
+        else if (viz != False)
+            return false;
+
+        return true;
+    }
+}
