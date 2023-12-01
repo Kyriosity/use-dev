116ad289e358a3aefabd38c2233faffe1ea183dd
Kiryl Drougakov <kiryl_d@gmx.net>
2023-12-01 22:02:23 +0100

    N-Val update (obsolete)

--- src/TuttiFrutti/N-Val/IFacetValue.cs
+++ src/TuttiFrutti/N-Val/IFacetValue.cs
@@ -1,10 +1,15 @@
 ﻿namespace N_Val;
 
-public interface IMultifacetValue<T> { }
+public interface IMultifacetValue<T>
+{
+
+    //public T this[U unit] => Get(unit);
+}
 
 public interface IValueByUnit<T, U> : IMultifacetValue<T> where U : Enum
 {
     public T Get(out U unit);
     public T Get(U unit);
+
     public void Set(T value, U unit);
 }
