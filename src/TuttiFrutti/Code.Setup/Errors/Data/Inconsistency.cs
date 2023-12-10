0de3418f8646de36fca1977c53f101010b7e7828
Kiryl Drougakov <kiryl_d@gmx.net>
2023-12-10 16:07:32 +0100

    custom extensions updated

---
+++ src/TuttiFrutti/Code.Setup/Errors/Data/Inconsistency.cs
@@ -0,0 +1,7 @@
+﻿namespace Abc.Ext.Errors.Data;
+public class Inconsistency : Exception
+{
+    public Inconsistency(string? message = "", Exception? inner = null) : base(message, inner) { }
+
+    public static dynamic Throw(string? message = "", Exception? inner = null) => throw new Inconsistency(message, inner);
+}
