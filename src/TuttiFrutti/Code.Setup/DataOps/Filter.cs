c36267f27d7f470ef6fcfc1ae9fad50544b839ba
Kiryl Drougakov <kiryl_d@gmx.net>
2024-01-09 18:23:05 +0100

    custom query extension

---
+++ src/TuttiFrutti/Code.Setup/DataOps/Filter.cs
@@ -0,0 +1,14 @@
+﻿namespace Abc.Ext.DataOps;
+public static class Filter
+{
+    public static IEnumerable<T> ByCast<T>(IEnumerable<object> source, out IEnumerable<object> unmatched) where T : class {
+
+        var casted = source.Select((x, i) => (val: x as T, index: i)).Where(g => g.val is not null)
+            .ToList();
+
+        var indeces = casted.Select(x => x.index).ToList();
+        unmatched = source.Where((_, i) => !indeces.Contains(i));
+
+        return casted.Select(x => x.val);
+    }
+}
