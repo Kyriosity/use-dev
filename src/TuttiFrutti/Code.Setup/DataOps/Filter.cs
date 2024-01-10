96ccee62615c59ce132da0cca68aa9e5ddee4edb
Kiryl Drougakov <kiryl_d@gmx.net>
2024-01-10 21:31:56 +0100

    method expanded for index

--- src/TuttiFrutti/Code.Setup/DataOps/Filter.cs
+++ src/TuttiFrutti/Code.Setup/DataOps/Filter.cs
@@ -3,12 +3,22 @@ public static class Filter
 {
     public static IEnumerable<T> ByCast<T>(IEnumerable<object> source, out IEnumerable<object> unmatched) where T : class {
 
+        var casted = ByCast<T>(source, out int[] indeces);
+
+        unmatched = source.Where((_, i) => !indeces.Contains(i));
+
+        return casted;
+    }
+
+    public static IEnumerable<T> ByCast<T>(IEnumerable<object> source, out int[] matchIndeces) where T : class {
         var casted = source.Select((x, i) => (val: x as T, index: i)).Where(g => g.val is not null)
             .ToList();
 
-        var indeces = casted.Select(x => x.index).ToList();
-        unmatched = source.Where((_, i) => !indeces.Contains(i));
+        matchIndeces = casted.Select(x => x.index).ToArray();
 
         return casted.Select(x => x.val);
+    }
+
+    public static void ByCast<T>(IEnumerable<object> source, Action selector, out int[] matchIndeces) where T : class {
     }
 }
