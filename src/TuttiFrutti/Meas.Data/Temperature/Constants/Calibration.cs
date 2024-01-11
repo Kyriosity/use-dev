fa40d17a1471bf8e70011c19c459e487185efbb8
Kiryl Drougakov <kiryl_d@gmx.net>
2024-01-11 18:44:24 +0100

    meas data updated/extended

---
+++ src/TuttiFrutti/Meas.Data/Temperature/Constants/Calibration.cs
@@ -0,0 +1,32 @@
+﻿using Meas.Data.Setup.Attributes;
+
+namespace Meas.Data.Temperature;
+public partial class Constants
+{
+    public const string AbsZero = "Absolute zero";
+    public const string AbsoluteZero = "Absolute zero";
+
+    [Precision(1.0)]
+    Rec Extremes = [
+        (long.MinValue, Fahrenheit, "=@max", (long)-5_124_095_576_030_431_232, Celsius),
+        (long.MinValue, Fahrenheit, "=@max", (long)-5_124_095_576_030_431_232, Celsius),
+
+        (uint.MaxValue, Fahrenheit, "=@max", (uint)2_386_092_924, Celsius),
+        (int.MaxValue, F, "=@max", (int)1_193_046_453, C),
+        (int.MinValue, F, "=@max", (int)-1_193_046_488, C),
+
+        (short.MaxValue, Fahrenheit, "=@max", (short)18_186, C),
+
+        (byte.MaxValue, F, "=@max", (byte)124, C),
+
+        (sbyte.MaxValue, F, "=@max", (sbyte)53, C),
+        (sbyte.MinValue, F, "=@max", (sbyte)-89, C),
+    ];
+
+    [Precision(.000_001)]
+    Dir ControlPoints = new() {
+        ["0 Celcius"] = [(0, Celsius), (273.15, Kelvin), (32, F)],
+        ["0 Fahrenheit"] = [(0, Fahrenheit), (-17.777_8, Celsius), (255.372_2, Kelvin)],
+        ["C-F intersect"] = [(-40, C), (-40, F)],
+    };
+}
--- src/TuttiFrutti/Meas.Data/Temperature/PhaseChange/Ionization.cs
+++ src/TuttiFrutti/Meas.Data/Temperature/PhaseChange/Ionization.cs
@@ -1,11 +1,10 @@
-﻿using Meas.Data.Setup.Attributes;
-namespace Meas.Data.Temperature;
+﻿namespace Meas.Data.Temperature;
 
 public partial class PhaseChange
 {
-    [NotForTest]
     Dir Ionization = new() {
-        //[""] = [(0.01, Celsius), , (, Fahrenheit)],
+        ["Quark-gluon plasma, CERN 2012"] = [(5_500_000_000_000, Kelvin), (989_9999_999_54, Fahrenheit)],
     };
+
 
 }
