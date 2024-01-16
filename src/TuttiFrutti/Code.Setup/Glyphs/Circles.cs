4609c6695717c66278118198832e23f2cb456c6a
Kiryl Drougakov <kiryl_d@gmx.net>
2024-01-16 15:36:27 +0100

    #add: glyphs

--- src/TuttiFrutti/Code.Setup/Glyphs/Circles.cs
+++ src/TuttiFrutti/Code.Setup/Glyphs/Circles.cs
@@ -1,14 +1,26 @@
 ﻿namespace Abc.Ext.Glyphs;
 public static class Circles
 {
-    public class Small
+    // wikipedia.org/wiki/Circle_symbol#Unicode
+    public static class Small
     {
+
         public const char Black = '\u25CF';
         public const char HalfFilled_Hor = '\u25D2';
         public const char HalfFilled_Vert = '\u25D0';
+        public const char QuadrantFilled = '\u25D4';
+        public const char ThreeQuadrantFilled = '\u25D5';
+
     }
+
+    public static class Medium
+    {
+        public const char Black = '\u26AB';
+    }
+
     public static class Big
     {
+        public const char Heavy = '\u2B55';
         public const char Void = '\u26AA';
         public const char Black = '\u26AB';
 
---
+++ src/TuttiFrutti/Code.Setup/Glyphs/Typo.cs
@@ -0,0 +1,5 @@
+﻿namespace Abc.Ext.Glyphs;
+public static class Typo
+{
+    public const char Paragraph = '¶';
+}
