2d679f17b3fa6df5ecabaf551e305b1860bfc441
Kiryl Drougakov <kiryl_d@gmx.net>
2024-02-05 21:08:30 +0100

    #adjust: enumerables

--- src/TuttiFrutti/Code.Setup/DataOps/Combi.cs
+++ src/TuttiFrutti/Code.Setup/DataOps/Combi.cs
@@ -3,12 +3,14 @@
 namespace Abc.Ext.DataOps;
 public static class Combi
 {
-    public static IEnumerable<T[]> Pair<T>(IEnumerable<T> data) =>
-         from left in data.Select((x, index) => new { x, index })
-         from right in data.Select((x, index) => new { x, index })
-         where left.index < right.index
-         select new T[] { left.x, right.x };
+    public static IEnumerable<T[]> Pair<T>(IEnumerable<T> source) {
+        var list = source.ToList();
+        return from left in list.Select((x, index) => new { x, index })
+               from right in list.Select((x, index) => new { x, index })
+               where left.index < right.index
+               select new T[] { left.x, right.x };
+    }
 
     public static IEnumerable<T[]> Triple<T>(IEnumerable<T> _) => NotImplemented.Throw("on demand");
-    // ToDo: quadruple, quintuple, sextuple, septuple, octuple, ..., n‑tuple,
+    // ToAdd: quadruple, quintuple, sextuple, septuple, octuple, ..., n‑tuple,
 }
