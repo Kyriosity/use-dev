78b0998ee7a60b42cbeee05b156b66453a37380b
Kiryl Drougakov <kiryl_d@gmx.net>
2024-02-14 13:55:29 +0100

    #fix, #tiny: wrong units type

--- src/TuttiFrutti/FuncStore.Convert.Tests/Digital/BinaryPrefixedTests.cs
+++ src/TuttiFrutti/FuncStore.Convert.Tests/Digital/BinaryPrefixedTests.cs
@@ -2,12 +2,12 @@
 using FuncStore.Conversion.Tests.Setup.Metadata;
 using FuncStore.Conversion.Tests.Setup.Steps;
 using MeasData.Digital;
-using In = MeasUnits.Prefixes.Binary.Exponent.In;
+using Bin = MeasUnits.Prefixes.Binary;
 
 namespace FuncStore.Conversion.Tests.Digital;
 
-[TestBy<Factoring<In>>]
-[TestBy<Exponentiation<In>>]
+[TestBy<Factoring<Bin.In>>]
+[TestBy<Exponentiation<Bin.Exponent.In>>]
 internal class BinaryPrefixedTests<TStore, TUnit> : ArrangeActAssert<TStore, TUnit>
     where TStore : IFuncStore<TUnit>, new() where TUnit : Enum
 {
