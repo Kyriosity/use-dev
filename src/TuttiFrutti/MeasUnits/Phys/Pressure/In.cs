ad64e1d258086fc6db634c171058d0553ff8f2ca
Kiryl Drougakov <kiryl_d@gmx.net>
2024-03-09 20:21:29 +0100

    #add, #small: units of pressure

---
+++ src/TuttiFrutti/MeasUnits/Phys/Pressure/In.cs
@@ -0,0 +1,24 @@
+﻿namespace MeasUnits.Phys.Pressure;
+public enum In
+{
+    Undefined = 0,
+
+    //[Alias(Pa)]
+    Pascal = 1,
+
+    bar,
+
+    standardAtmosphere,
+    technicalAtmosphere,
+
+    //[Alias(Torr)]
+    torr, // Evangelista Torricelli, barometer principle, 1644. // 1 Torr = 0.999999857533699 mmHg
+    millimetersMercury,
+
+    inchesMercury, // (inHg),
+
+    //[Alias(PSI)]
+    poundPerSquareInch
+}
+
+
