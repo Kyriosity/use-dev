10d49de4c5fbd8caba0ba18a0d0381e1a4fdaba8
Kiryl Drougakov <kiryl_d@gmx.net>
2024-03-11 15:56:41 +0100

    #tiny, #to-design: Alias check

--- src/TuttiFrutti/AbcExt/Metadata/Wording/AliasAttribute.cs
+++ src/TuttiFrutti/AbcExt/Metadata/Wording/AliasAttribute.cs
@@ -7,6 +7,11 @@ public class AliasAttribute : Attribute
     public AliasAttribute(string otherName, params string[] otherNames) {
 
         string[] aliases = [otherName, .. otherNames];
+
+
+        // ToDesign: wording utilites and multi-Argument with reporting
+        // Argument.ThrowIfAny(Wording.NoLeadTrail, Wording.Normal, aliases);
+
         // Argument.ThrowIfAny(non_mean, names); // ILLEGAL LEAD. TRAIL
         DuplicatedArgument.ThrowIfAny(Equal.Loose(), aliases);
 
