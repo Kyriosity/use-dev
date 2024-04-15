fce552106645eeeda2889a8ef0ac6cc374b97a28
Kiryl Drougakov <kiryl_d@gmx.net>
2024-04-15 17:22:23 +0200

    #mini-part: accuracy (of measurement)

---
+++ src/TuttiFrutti/Abc/Accuracy/Confidence.cs
@@ -0,0 +1,13 @@
+﻿namespace Abc.Accuracy;
+public class Confidence<N> : IConfidence_Init<N> where N : INumber<N>
+{
+    public N Interval { get; init; } = N.Zero;
+}
+
+public class Confidence_Edit<N> : IConfidence_Edit<N> where N : INumber<N>
+{
+    public N Interval { get; set; } = N.Zero;
+}
+
+public class Confidence : Confidence<byte>;
+public class Confidence_Edit : Confidence_Edit<byte>;
---
+++ src/TuttiFrutti/Abc/Accuracy/IConfidence.cs
@@ -0,0 +1,19 @@
+﻿namespace Abc.Accuracy;
+public interface IConfidence<N> where N : INumber<N>
+{
+    N Interval { get; }
+}
+
+
+public interface IConfidence_Init<N> : IConfidence<N>
+    where N : INumber<N>
+{
+    new N Interval { get; init; }
+}
+
+public interface IConfidence_Edit<N> : IConfidence<N>
+    where N : INumber<N>
+{
+    new N Interval { get; set; }
+}
+
