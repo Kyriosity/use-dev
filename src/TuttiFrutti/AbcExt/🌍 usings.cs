e9a9c4288e399ebd74555fda3dc4de37ed405939
Kiryl Drougakov <kiryl_d@gmx.net>
2024-05-27 14:47:47 +0200

    #fix: small syntax errors

--- src/TuttiFrutti/AbcExt/🌍 usings.cs
+++ src/TuttiFrutti/AbcExt/🌍 usings.cs
@@ -3,4 +3,4 @@ global using AbcExt.Errors.Argument;
 global using AbcExt.Errors.Basal;
 global using AbcExt.Errors.Sys;
 global using System.Numerics;
-global using ArgExpr = System.Runtime.CompilerServices.CallerArgumentExpressionAttribute
+global using ArgExpr = System.Runtime.CompilerServices.CallerArgumentExpressionAttribute;
--- src/TuttiFrutti/AbcStructTests/🌍 usings.cs
+++ src/TuttiFrutti/AbcStructTests/🌍 usings.cs
@@ -1,4 +1,5 @@
 global using AbcExt.Errors.Data;
+global using AbcExt.Errors.Sys;
 global using AbcStruct.Heap.Core;
 global using AbcStruct.Heap.Multitype;
 global using NUnit.Framework;
