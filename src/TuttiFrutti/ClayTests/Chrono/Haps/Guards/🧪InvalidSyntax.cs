1164e4e4392766c8abc12ed55d449c9edf4e7b11
Kiryl Drougakov <kiryl_d@gmx.net>
2024-05-27 14:45:30 +0200

    🧪add: tests for AbcCHRONO

---
+++ src/TuttiFrutti/ClayTests/Chrono/Haps/Guards/🧪InvalidSyntax.cs
@@ -0,0 +1,22 @@
+﻿namespace ClayTests.Chrono.Haps.Guards;
+public class InvalidSyntax
+{
+    [Test]
+    public void Calls() {
+        /// next statements must NOT compile
+        /* Era.Common.Millennium(1);
+        Era.BeforeCommon.Decamillenniuum(1);
+        Hist.Human.Year(1);
+        Hist.Eon.Century(1); */
+    }
+
+    [Test]
+    public void NegativeValues() {
+        /// next statements must NOT compile
+       /* Era.Common.Year(-1);
+        Era.Common.At.August(-1);
+
+        Hist.Eon.Megaannum(-1);
+        Hist.Eon.Ago.Megaannum(-1); */
+    }
+}
--- src/TuttiFrutti/ClayTests/Chrono/Haps/Guards/🧪NumberRange.cs
+++ src/TuttiFrutti/ClayTests/Chrono/Haps/Guards/🧪NumberRange.cs
@@ -2,10 +2,39 @@
 public class NumberRange
 {
     [Test]
-    public void Years() {
-        var era = Era.Common;
-        var hap = era.Year(2024);
+    public void NoZeroDate() {
+        var at = Era.Common.At;
+        Assert.Throws<ArgumentOutOfRangeException>(() => _ = at.January(0));
+        Assert.Throws<ArgumentOutOfRangeException>(() => _ = at.circa().August(0));
+    }
 
-        Assert.Throws<NotSet>(() => _ = hap.UnixYear);
+
+    [Test]
+    public void NoZeroYears() {
+        Assert.Throws<ArgumentOutOfRangeException>(() => _ = Era.Common.Year(0));
+        Assert.Throws<ArgumentOutOfRangeException>(() => _ = Era.BeforeCommon.circa().Year(0));
+
+        Era.Holocene.Year(0);
+        Era.Holocene.circa().Year(0);
+    }
+
+    [Test]
+    public void LeapYears() {
+        var at = Era.Common.At;
+        Assert.That(at.February(29).Year(2024), Is.Not.Null);
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => _ = at.February(29).Year(2023));
+    }
+
+    [Test]
+    public void TrespassingDates() {
+        var at = Era.Common.At;
+
+        Assert.That(at.November(30).Year(2021), Is.Not.Null);
+        Assert.Throws<ArgumentOutOfRangeException>(() => _ = at.November(31).Year(2021));
+
+        Assert.That(at.December(31).Year(2021), Is.Not.Null);
+        Assert.Throws<ArgumentOutOfRangeException>(() => _ = at.December(32).Year(2021));
+
     }
 }
