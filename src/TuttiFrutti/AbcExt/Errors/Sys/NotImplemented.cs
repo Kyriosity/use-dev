0d9637dc0e93892b4e02313a2f8d9914b9dc2a8a
Kiryl Drougakov <kiryl_d@gmx.net>
2024-06-05 11:24:43 +0200

    Enhance: Clay > NotImplemented excetpion

--- src/TuttiFrutti/AbcExt/Errors/Sys/NotImplemented.cs
+++ src/TuttiFrutti/AbcExt/Errors/Sys/NotImplemented.cs
@@ -1,8 +1,12 @@
-﻿namespace AbcExt.Errors.Sys;
+﻿using System.Data.SqlTypes;
+
+namespace AbcExt.Errors.Sys;
 public class NotImplemented : AbcException<NotImplementedException>
 {
     private NotImplemented() { /* to prevent direct instantiation */ }
 
     public static dynamic ToDo(string message) => true;
+    public static T ToDo<T>(string message) where T : INullable => ToDo<T>(default, message);
+    public static T ToDo<T>(T stub, string message) => stub;
 }
 
