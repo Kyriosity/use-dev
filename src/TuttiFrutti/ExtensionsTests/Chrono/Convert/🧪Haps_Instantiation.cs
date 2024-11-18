22ae6709f0c13f1d0768cc3615d349cda7faa0fa
Kiryl Drougakov <kiryl_d@gmx.net>
2024-11-18 20:29:56 +0100

    🧪non relevant test to move

--- src/TuttiFrutti/ExtensionsTests/Chrono/Convert/🧪Haps_Instantiation.cs
+++ src/TuttiFrutti/ExtensionsTests/Chrono/Convert/🧪Haps_Instantiation.cs
@@ -2,10 +2,12 @@
 public class Haps_Instantiation
 {
     [Test]
+    [Ignore("Not relevant")]
     public void RenderFromDemo() {
+
+        // These are model tests
         var book = Creatures.Persons["da Vinci"];
         book = Creatures.Persons["Raphael"];
 
-        Assert.Ignore("Stuff expected");
     }
 }
