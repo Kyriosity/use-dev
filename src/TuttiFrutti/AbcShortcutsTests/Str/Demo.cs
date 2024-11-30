be7b01ba690a2a06db5998129c85a33a1b03b85f
Kiryl Drougakov <kiryl_d@gmx.net>
2024-11-30 22:10:53 +0100

    First definition of string shortcuts

---
+++ src/TuttiFrutti/AbcShortcutsTests/Str/Demo.cs
@@ -0,0 +1,48 @@
+﻿using AbcCommu.Errors.Sys;
+
+namespace AbcShortcutsTests.Str;
+
+public class Demo
+{
+    [Test]
+    public void SyntaxOnly() {
+        var smth = "NOT NULLL TEST STRING";
+
+        // SHORTCATS
+        _ = smth.Is().NullOrWhitespace;
+        _ = smth.Not().NullOrWhitespace;
+
+        // NEW
+        _ = smth.Is().Ascii;
+        _ = smth.Is().Latin;
+    }
+
+
+
+    [Test]
+    public void Extensions() {
+        var sample = "               ";
+        Assert.That(sample.Not().NullOrEmpty && sample.Is().NullOrWhitespace, Is.True);
+
+
+    }
+}
+
+public static class StrExtensions_Traits
+{
+    public static ITraits Is(this string subj) => NotImplemented.ToDo("Is stub");
+    public static ITraits Not(this string subj) => NotImplemented.ToDo("Is stub");
+}
+
+
+public interface ITraits
+{
+    bool NullOrEmpty { get; }
+    bool NullOrWhitespace { get; }
+    bool NullOrGrayspace { get; }
+
+    bool Ascii { get; }
+
+    bool Latin { get; }
+    bool LatinOrNumeric { get; }
+}
