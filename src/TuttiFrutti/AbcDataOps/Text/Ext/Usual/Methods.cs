704fb95d8fb8544006fef47e3a4f218d9bc4ade7
Kiryl Drougakov <kiryl_d@gmx.net>
2024-12-23 19:30:43 +0100

    ⭐I S i e⭐ INITIAL COMMIT: usual (classical) string extensions

---
+++ src/TuttiFrutti/AbcDataOps/Text/Ext/Usual/Methods.cs
@@ -0,0 +1,7 @@
+﻿namespace AbcDataOps.Text.Ext.Usual;
+public static class Methods
+{
+    public static bool IsNullOrEmpty(this string? value) => string.IsNullOrEmpty(value);
+    public static bool IsNullOrWhiteSpace(this string? value) => string.IsNullOrWhiteSpace(value);
+    public static string? IsInterned(this string value) => string.IsInterned(value);
+}
---
+++ src/TuttiFrutti/AbcShortcutsTests/Text/Usual/Demo.cs
@@ -0,0 +1,41 @@
+﻿using AbcDataOps.Text.Ext.Usual;
+
+namespace AbcShortcutsTests.Text.Usual;
+
+public class Demo
+{
+    private const string multilineWhitespaces = @"   
+    
+   ";
+
+    private const string tabAmongSpaces = "   \t  ";
+
+
+    [Test]
+    public void BlankTraits() {
+        Assert.That(
+            string.Empty.IsNullOrWhiteSpace() && multilineWhitespaces.IsNullOrWhiteSpace() && tabAmongSpaces.IsNullOrWhiteSpace()
+            && "".IsNullOrEmpty() && "         ".IsNullOrWhiteSpace(),
+        Is.True);
+
+        Assert.That(
+            " LOREM ipsum  ".IsNullOrWhiteSpace() || " ".IsNullOrEmpty(),
+            Is.False);
+    }
+
+
+    [Test]
+    public void BlankTraits_Changed() {
+        var textToModify = string.Empty;
+
+        textToModify += "  ";
+        Assert.That(
+            textToModify.IsNullOrWhiteSpace() && !textToModify.IsNullOrEmpty(),
+            Is.True);
+
+        textToModify += ".";
+        Assert.That(
+            textToModify.IsNullOrWhiteSpace(),
+            Is.False);
+    }
+}
