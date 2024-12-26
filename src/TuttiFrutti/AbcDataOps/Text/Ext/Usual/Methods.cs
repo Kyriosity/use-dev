72b9efdbf4e11edaa5764d41e0a6348dfe77e813
Kiryl Drougakov <kiryl_d@gmx.net>
2024-12-26 18:32:19 +0100

    ⭐I S I e ⭐ plus methods for string blank

--- src/TuttiFrutti/AbcDataOps/Text/Ext/Usual/Methods.cs
+++ src/TuttiFrutti/AbcDataOps/Text/Ext/Usual/Methods.cs
@@ -4,4 +4,11 @@ public static class Methods
     public static bool IsNullOrEmpty(this string? value) => string.IsNullOrEmpty(value);
     public static bool IsNullOrWhiteSpace(this string? value) => string.IsNullOrWhiteSpace(value);
     public static string? IsInterned(this string value) => string.IsInterned(value);
+
+}
+
+public static class Plus
+{
+    public static bool IsNullEmptyOrSingleSpace(this string? value) => string.IsNullOrEmpty(value) || " " == value;
+    public static bool IsNullEmptyOrSpaces(this string? value) => string.IsNullOrEmpty(value?.Trim());
 }
---
+++ src/TuttiFrutti/AbcShortcutsTests/Text/Usual/Plus/Demo.cs
@@ -0,0 +1,20 @@
+﻿using AbcDataOps.Text.Ext.Usual;
+
+namespace AbcShortcutsTests.Text.Usual.Plus;
+public class Demo
+{
+    [Test]
+    public void BlankTraits() {
+        const string nullText = null;
+
+        Assert.That(
+            nullText.IsNullEmptyOrSpaces() && "".IsNullEmptyOrSpaces() &&
+            " ".IsNullEmptyOrSpaces() && "    ".IsNullEmptyOrSpaces() && !"test".IsNullEmptyOrSpaces(),
+    Is.True);
+
+        Assert.That(
+            nullText.IsNullEmptyOrSingleSpace() && "".IsNullEmptyOrSingleSpace() &&
+            " ".IsNullEmptyOrSingleSpace() && !"    ".IsNullEmptyOrSingleSpace() && !"test".IsNullEmptyOrSingleSpace(),
+        Is.True);
+    }
+}
