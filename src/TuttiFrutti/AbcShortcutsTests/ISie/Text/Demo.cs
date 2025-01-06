f916e2b443e29789ff14d4b9454af989c432dc2a
Kiryl Drougakov <kiryl_d@gmx.net>
2025-01-06 18:52:48 +0100

    ⭐I S i e⭐ text initial demo, filling w/🧪

---
+++ src/TuttiFrutti/AbcShortcutsTests/ISie/Text/Demo.cs
@@ -0,0 +1,18 @@
+﻿using AbcDataOps.ISie;
+
+namespace AbcShortcutsTests.ISie.Text;
+public class DemoIs
+{
+    [Ignore("syntax demo which can fail before implemented")]
+    public void Filling() {
+        bool @is; var text = "doesnt matter";
+        @is = text.Is().NullOr.Empty;
+        @is = text.Is().NullOr.EmptyOr.Whitespace;
+        @is = text.Is().NullOr.EmptyOr.SingleSpace;
+        @is = text.Is().NullOr.EmptyOr.Spaces;
+
+        @is = text.Is().EmptyOr.SingleSpace; // null will throw here
+        @is = text.Is().EmptyOr.Whitespace;
+        @is = text.Is().EmptyOr.Spaces;
+    }
+}
