c33e6c997a3111f1eb32e595e51ce3991504d954
Kiryl Drougakov <kiryl_d@gmx.net>
2025-01-12 17:12:16 +0100

    🧪arrangements

---
+++ src/TuttiFrutti/AbcShortcutsTests/⚙️setup/Arrange.cs
@@ -0,0 +1,6 @@
+﻿namespace AbcShortcutsTests.Setup;
+public abstract class Arrange
+{
+    protected bool WillBeTrue;
+    protected bool WillBeFalse;
+}
--- src/TuttiFrutti/AbcShortcutsTests/🌐usings.cs
+++ src/TuttiFrutti/AbcShortcutsTests/🌐usings.cs
@@ -1,1 +1,3 @@
-﻿global using AbcExtNUnit.Metadata.Shortcuts.Test;
+﻿global using AbcDataOps.ISie;
+global using AbcDataOps.ISie.Lingua;
+global using AbcExtNUnit.Metadata.Shortcuts.Test;
