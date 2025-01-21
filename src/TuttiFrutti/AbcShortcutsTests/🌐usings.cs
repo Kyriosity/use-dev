21a8eda8a69c6dfb57e3e7d000150967a0a185e4
Kiryl Drougakov <kiryl_d@gmx.net>
2025-01-21 13:34:30 +0100

    ⭐ISie🧪drafting demos

--- src/TuttiFrutti/AbcShortcutsTests/🌐usings.cs
+++ src/TuttiFrutti/AbcShortcutsTests/🌐usings.cs
@@ -1,6 +1,2 @@
-﻿global using Abc.Lingua.indo_euro.balto_slavic.slavic.south;
-global using Abc.Lingua.indo_euro.hellenic;
-global using Abc.Lingua.japonic;
-global using Abc.Lingua.turkic.east;
-global using AbcDataOps.ISie;
+﻿global using AbcDataOps.ISie;
 global using AbcExtNUnit.Metadata.Shortcuts.Test;
