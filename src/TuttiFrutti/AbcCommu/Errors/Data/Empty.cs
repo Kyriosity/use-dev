972ee67f96400e844d6f79d281fbfeb337fbacb2
Kiryl Drougakov <kiryl_d@gmx.net>
2025-02-03 18:39:05 +0100

    🧱 + custom Empty exception

---
+++ src/TuttiFrutti/AbcCommu/Errors/Data/Empty.cs
@@ -0,0 +1,13 @@
+﻿namespace AbcCommu.Errors.Data;
+
+public class Empty(string message) : Exception<NotSet>(message)
+{
+    public static bool ThrowIf<T>(IEnumerable<T> coll, [ArgExpr(nameof(coll))] string expr = "") =>
+        coll.Any() ? false : throw new Empty(expr);
+
+    public static bool ThrowIf(string value, [ArgExpr(nameof(value))] string expr = "")
+        => string.Empty == value ? false : throw new Empty(expr);
+
+    public static bool ThrowIfWhitespace(string value, [ArgExpr(nameof(value))] string expr = "") =>
+        string.IsNullOrWhiteSpace(value) ? throw new Empty(expr) : false;
+}
