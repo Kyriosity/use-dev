cdf2c50f7151b1f827f71b2a8a825bca4a65ed63
Kiryl Drougakov <kiryl_d@gmx.net>
2025-02-09 22:50:12 +0100

    Builders: allow convert to expected result type

---
+++ src/TuttiFrutti/Abc/Logic/Build/ConvertableToResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Abc.Logic.Build;
+public abstract class ConvertableToResult<T> : IResultReady
+{
+    public static implicit operator T(ConvertableToResult<T> source) =>
+        throw new NotImplementedException($"{source.GetType().FullName} to {typeof(T).FullName}");
+}
+
+public interface IResultReady;
