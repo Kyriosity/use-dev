18f756b6d401970cd59811342a32f87acf026566
Kiryl Drougakov <kiryl_d@gmx.net>
2025-02-12 20:45:17 +0100

    🧪[TestOf] attribute 🪥 refined

--- src/TuttiFrutti/AbcExtNUnit/Metadata/Shortcuts/Source/CompileTestSource/TestOfAttribute.cs
+++ src/TuttiFrutti/AbcExtNUnit/Metadata/Shortcuts/Source/CompileTestSource/TestOfAttribute.cs
@@ -1,13 +1,11 @@
-﻿namespace AbcExtNUnit.Metadata.Shortcuts.Source;
+﻿namespace AbcExtNUnit.Metadata.Shortcuts.Source.CompileTestSource;
 
 public class TestOfAttribute<T>(params string[] args) : TestOfAttribute<T, Stubs.Basic>(args);
 public class TestOfAttribute<T1, T2>(params string[] args) : TestOfAttribute<T1, T2, Stubs.Basic>(args);
 public class TestOfAttribute<T1, T2, T3>(params string[] args) : TestOfAttribute<T1, T2, T3, Stubs.Basic>(args);
 public class TestOfAttribute<T1, T2, T3, T4>(params string[] args) : TestOfAttribute<T1, T2, T3, T4, Stubs.Basic>(args);
-public class TestOfAttribute<T1, T2, T3, T4, T5>(params string[] args)
-    : TestOfAttribute<T1, T2, T3, T4, T5, Stubs.Basic>(args);
-public class TestOfAttribute<T1, T2, T3, T4, T5, T6>(params string[] args)
-    : TestOfAttribute<T1, T2, T3, T4, T5, T6, Stubs.Basic>(args);
+public class TestOfAttribute<T1, T2, T3, T4, T5>(params string[] args) : TestOfAttribute<T1, T2, T3, T4, T5, Stubs.Basic>(args);
+public class TestOfAttribute<T1, T2, T3, T4, T5, T6>(params string[] args) : TestOfAttribute<T1, T2, T3, T4, T5, T6, Stubs.Basic>(args);
 public class TestOfAttribute<T1, T2, T3, T4, T5, T6, T7>(params string[] args)
     : TestOfAttribute<T1, T2, T3, T4, T5, T6, T7, Stubs.Basic>(args);
 public class TestOfAttribute<T1, T2, T3, T4, T5, T6, T7, T8>(params string[] args)
@@ -18,21 +16,19 @@ public class TestOfAttribute<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(params str
     : TestOfAttribute<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, Stubs.Basic>(args);
 
 public class TestOfAttribute<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(params string[] args)
-    : TestCaseSourceAttribute(HardCodedMethodName, new object[] { SuppliedTypes, args })
+    : TestCaseSourceAttribute(HardCodedMethodName, [SuppliedTypes, args])
 {
     static IEnumerable<Type> SuppliedTypes => _suppliedTypes ?? _typeParameters
         .TakeWhile(x => x != _ofTypeStub).ToArray();
 
     private static readonly IEnumerable<Type>? _suppliedTypes = null;
 
-    private static readonly Type[] _typeParameters = new Type[] { typeof(T1), typeof(T2), typeof(T3),
-        typeof(T4), typeof(T5), typeof(T6), typeof(T7), typeof(T8), typeof(T9), typeof(T10), typeof(T11) };
+    private static readonly Type[] _typeParameters = [ typeof(T1), typeof(T2), typeof(T3),
+        typeof(T4), typeof(T5), typeof(T6), typeof(T7), typeof(T8), typeof(T9), typeof(T10), typeof(T11) ];
 
     private static readonly Type _ofTypeStub = typeof(Stubs.Basic);
 
-    // should supply through context/properties but can't because of NUnit issues:
-    // e.g. - github.com/nunit/nunit/issues/548
+    // should be supplied through context/properties but can't because of NUnit issues:
+    // e.g. - github.com/nunit/nunit/issues/548 -- still open in 2025
     protected const string HardCodedMethodName = "CompileTestSource";
 }
-
-
--- src/TuttiFrutti/MeasData/🌐usings.cs
+++ src/TuttiFrutti/MeasData/🌐usings.cs
@@ -1,5 +1,4 @@
-﻿global using AbcCommu.Errors.Sys;
-global using AbcRefl.Metadata;
+﻿global using AbcRefl.Metadata;
 global using System.Numerics;
 global using Dir = System.Collections.Generic.Dictionary<string, System.ValueTuple<double, string>[]>;
 global using Rec = (object leftVal, string leftUnit, string note, object rightVal, string rightUnit)[];
