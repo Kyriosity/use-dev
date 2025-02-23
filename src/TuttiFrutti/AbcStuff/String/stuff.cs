929ef9ad4343952f8111c5ed51cd0fd6532efa36
Kiryl Drougakov <kiryl_d@gmx.net>
2025-02-23 21:29:40 +0100

    🖌️AbcStuff

--- src/TuttiFrutti/AbcStuff/String/stuff.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AbcStuff.String;
-#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
-public static class space
-#pragma warning restore CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
-{
-    public const string Single = " ";
-}
