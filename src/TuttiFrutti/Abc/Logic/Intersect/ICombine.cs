5068f5e8a03422d42f11030d40c2344cf8a45d4f
Kiryl Drougakov <kiryl_d@gmx.net>
2025-03-07 23:59:27 +0100

    AbcLogic: draft interface

---
+++ src/TuttiFrutti/Abc/Logic/Intersect/ICombine.cs
@@ -0,0 +1,14 @@
+﻿namespace Abc.Logic.Intersect;
+
+public interface ICombine<T>
+{
+
+    T Join(T op1, T op2);
+}
+
+public interface IBoolCombine : ICombine<bool>;
+
+public interface IDisjuct : IBoolCombine
+{
+    new bool Join(bool op1, bool op2) => op1 || op2;
+}
