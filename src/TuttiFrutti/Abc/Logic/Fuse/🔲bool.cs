336f81434e9f086cea0729a97d972f787f140a98
Kiryl Drougakov <kiryl_d@gmx.net>
2025-03-08 16:36:23 +0100

    🖌️AbcLogic: naming

---
+++ src/TuttiFrutti/Abc/Logic/Fuse/🔲bool.cs
@@ -0,0 +1,18 @@
+﻿namespace Abc.Logic.Fuse;
+
+public interface IBool : IBase<bool>;
+
+public interface IDisjunct : IBool
+{
+    new bool Join(bool op1, bool op2) => op1 | op2;
+}
+
+public interface IConjunct : IBool
+{
+    new bool Join(bool op1, bool op2) => op1 & op2;
+}
+
+public interface IExclude : IBool
+{
+    new bool Join(bool op1, bool op2) => op1 ^ op2;
+}
---
+++ src/TuttiFrutti/Abc/Logic/Fuse/🫚base.cs
@@ -0,0 +1,7 @@
+﻿namespace Abc.Logic.Fuse;
+
+public interface IBase<T>
+{
+    T Join(T op1, T op2);
+}
+
