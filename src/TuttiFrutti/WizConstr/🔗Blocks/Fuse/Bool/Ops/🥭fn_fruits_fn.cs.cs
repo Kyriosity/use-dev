d6a67c30a45c0276d9ead1e5839dfb0c8af54e97
Kiryl Drougakov <kiryl_d@gmx.net>
2025-03-12 13:37:52 +0100

    WizConstr: + bool Fn_🥭_Fn

---
+++ src/TuttiFrutti/WizConstr/🔗Blocks/Fuse/Bool/Ops/🥭fn_fruits_fn.cs.cs
@@ -0,0 +1,6 @@
+﻿namespace WizConstr.Blocks.Fuse.Bool.Ops;
+
+public abstract class Or_Fruit_Or<TSrc>(TSrc seed) : Fn_Fruit_Fn<TSrc, fuseFn.IDisjunct, fuseFn.IDisjunct>(seed);
+public abstract class Or_Fruit_And<TSrc>(TSrc seed) : Fn_Fruit_Fn<TSrc, fuseFn.IDisjunct, fuseFn.IConjunct>(seed);
+public abstract class And_Fruit_And<TSrc>(TSrc seed) : Fn_Fruit_Fn<TSrc, fuseFn.IConjunct, fuseFn.IConjunct>(seed);
+public abstract class And_Fruit_Or<TSrc>(TSrc seed) : Fn_Fruit_Fn<TSrc, fuseFn.IConjunct, fuseFn.IDisjunct>(seed);
