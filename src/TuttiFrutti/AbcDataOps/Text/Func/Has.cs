4586d4cdb22d98a4c38c7b7744455d3d4be47cfd
Kiryl Drougakov <kiryl_d@gmx.net>
2025-03-23 14:05:24 +0100

    AbcDataOps: predefined funcs GROUNDSTONE

---
+++ src/TuttiFrutti/AbcDataOps/Text/Func/Has.cs
@@ -0,0 +1,6 @@
+﻿namespace AbcDataOps.Text.Func;
+public class Has
+{
+    public static readonly Func<string, bool> ControlChars = subj =>
+        subj.ToCharArray().Any(char.IsControl);
+}
--- src/TuttiFrutti/AbcDataOps/🌐usings.cs
+++ src/TuttiFrutti/AbcDataOps/🌐usings.cs
@@ -1,5 +1,2 @@
-﻿global using Abc.Lingua;
-global using Abc.Logic;
-global using Abc.Marks;
+﻿global using Abc.Logic;
 global using System.Text.RegularExpressions;
-global using Or = Abc.Logic.Build.Disjuncts;
