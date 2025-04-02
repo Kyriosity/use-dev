feeeb0a59289f6d70f57893af0c9face483f5224
Kiryl Drougakov <kiryl_d@gmx.net>
2025-04-02 20:42:45 +0200

    ⚡Assert custom exception `+

---
+++ src/TuttiFrutti/AbcExtNUnit/Exceptions/Assertion.cs
@@ -0,0 +1,5 @@
+﻿namespace AbcExtNUnit.Exceptions;
+public class Assertion : AbcCommu.Errors.Basal.Exception<AssertionException>
+{
+    private Assertion() { /* to prevent direct instantiation */ }
+}
