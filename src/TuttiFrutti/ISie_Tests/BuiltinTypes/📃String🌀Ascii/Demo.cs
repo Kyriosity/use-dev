211ff21d9a2915098bc55c789683e2c7bf717143
Kiryl Drougakov <kiryl_d@gmx.net>
2025-04-23 10:43:35 +0200

    ⭐ISie⭐refine🧪

--- src/TuttiFrutti/ISie_Tests/BuiltinTypes/📃String🌀Ascii/Demo.cs
+++ src/TuttiFrutti/ISie_Tests/BuiltinTypes/📃String🌀Ascii/Demo.cs
@@ -1,24 +1,30 @@
 ﻿namespace ISie_Tests.BuiltinTypes.String_Encoding;
 public class Demo : Setup.Arrange
 {
-    [SyntaxDraft]
+#if DEBUG
+    [Test]
+#endif
     public void Ascii() {
-        True = "abc_123".Is().Ascii;
+        True = "abc\n123".Is().Ascii; // ToDo: ADD CONTRAL ASCII
         True = "abc_123".Is().Ascii.NoControls;
 
-        False = " \u1234 ".Is().Ascii;
+        False = " NOT\u1234ASCII".Is().Ascii;
+        False = "123😿579".Is().Ascii;
     }
 
-    [SyntaxDraft]
-    public void WithDisjunction() {
+#if DEBUG
+    [Test]
+#endif
+    public void WithPrecheck() {
         True = "abc_123".Is().NullEmptyOr.Ascii;
         _ = "abc_123".Is().EmptyOr.Ascii;
         _ = "abc_123".Is().Ascii.NoControls;
         _ = "abc_123".Is().NullEmptyOr.Ascii.NoControls;
 
     }
-
+#if DEBUG
     [Test]
+#endif
     public void TestWiz() {
 
         True = "START 1".Is().Ascii.NoControls;
