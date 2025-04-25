1cdffb357a5a71dc048abc15ff2a8c4019b88c20
Kiryl Drougakov <kiryl_d@gmx.net>
2025-04-25 14:04:21 +0200

    ⭐ISie⭐ > add: nullable char support

--- src/TuttiFrutti/ISie/Char/Go.cs
+++ src/TuttiFrutti/ISie/Char/Go.cs
@@ -1,8 +1,13 @@
-﻿using ISie.Char.Is.Menus;
+﻿using AbcCommu.Errors.Sys;
+using ISie.Char.Is.Menus;
 
 namespace ISie.Char;
 public static class Go
 {
-    public static IRoot Is(this char seed) => null;
+    public static IRoot Is(this char seed) => NotImplemented.Throw("coming soon...");
+    public static IRoot Is(this char? seed) => seed is null ? ArgumentNull.Throw(err_hint) : Is((char)seed);
     public static To.IMenu To(this char seed) => new To.Menu(seed);
+    public static To.IMenu To(this char? seed) => seed is null ? ArgumentNull.Throw(err_hint) : To((char)seed);
+
+    private const string err_hint = "You must check for null!";
 }
---
+++ src/TuttiFrutti/ISie_Eval/BuiltinTypes/🔣Char/✨Demo_Traps🚫.cs
@@ -0,0 +1,14 @@
+﻿namespace ISie_Eval.BuiltinTypes.Char;
+public class Demo_Traps : Setup.Arrange
+{
+    [Test]
+    public void You_Must_Check_for_Null() {
+        char? symb = 'W';
+        True = symb.Is().Letter;
+        True = 'w' == symb.To().Lower;
+
+        symb = null;
+        Error = ArgumentNull => symb.Is().Letter;
+        Error = ArgumentNull => symb.To().Lower;
+    }
+}
