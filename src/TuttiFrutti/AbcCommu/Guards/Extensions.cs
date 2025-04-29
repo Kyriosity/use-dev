be545254d407d2de49fa6f3e164b849e65b271c5
Kiryl Drougakov <kiryl_d@gmx.net>
2025-04-29 18:29:29 +0200

    ❗extensions to guard null and empty

---
+++ src/TuttiFrutti/AbcCommu/Guards/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using AbcCommu.Errors.Data;
+using AbcCommu.Errors.Sys;
+using System.Collections;
+
+namespace AbcCommu.Guards;
+public static class GuardExtensions
+{
+    public static T NotNull<T>(this T value, string message = "") =>
+        value is null ? NullReference.Throw(message) : value;
+
+    public static string NotEmpty(this string value, string message = "") =>
+        value is "" ? Empty.Throw(message) : value;
+    public static string NotEmpty(this ICollection value, string message = "") =>
+       value.Count is 0 ? Empty.Throw(message) : value;
+
+}
