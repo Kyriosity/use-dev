e28036352156bbcf2516a685592521113f4e7170
Kiryl Drougakov <kiryl_d@gmx.net>
2025-04-30 19:47:02 +0200

    AbcLogic > bit-wise logic operations -> boolean (to evaluate when required only)

--- src/TuttiFrutti/Abc/Logic/Fuse/🔲bool.cs
+++ src/TuttiFrutti/Abc/Logic/Fuse/🔲bool.cs
@@ -4,15 +4,16 @@ public interface IBool : IBase<bool>;
 
 public interface IDisjunct : IBool
 {
-    new bool Join(bool left, bool right) => left | right;
+    new bool Join(bool left, bool right) => left || right;
 }
 
 public interface IConjunct : IBool
 {
-    new bool Join(bool left, bool right) => left & right;
+    new bool Join(bool left, bool right) => left && right;
 }
 
 public interface IExclude : IBool
 {
     new bool Join(bool left, bool right) => left ^ right;
 }
+
