664dfe0f012391a96df42d883a95ad4bbdeab66a
Kiryl Drougakov <kiryl_d@gmx.net>
2025-05-05 17:46:01 +0200

    AbcExt: rename for shorter

--- src/TuttiFrutti/AbcCommu/Guards/Extensions.cs
+++ src/TuttiFrutti/AbcCommu/Guards/Extensions.cs
@@ -5,11 +5,11 @@ using System.Collections;
 namespace AbcCommu.Guards;
 public static class GuardExtensions
 {
-    public static T NotNull<T>(this T value, string message = "") =>
+    public static T NoNull<T>(this T value, string message = "") =>
         value is null ? NullReference.Throw(message) : value;
 
-    public static string NotEmpty(this string value, string message = "") =>
+    public static string NoEmpty(this string value, string message = "") =>
         value is "" ? Empty.Throw(message) : value;
-    public static string NotEmpty(this ICollection value, string message = "") =>
+    public static string NoEmpty(this ICollection value, string message = "") =>
        value.Count is 0 ? Empty.Throw(message) : value;
 }
