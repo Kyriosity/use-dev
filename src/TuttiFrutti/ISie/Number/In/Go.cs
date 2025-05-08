103f92921a1b6bf05eabe59bd085d87aa54c0ffc
Kiryl Drougakov <kiryl_d@gmx.net>
2025-05-08 21:36:35 +0200

    ⭐ISie⭐ layout `Number.In<>()` 🚧

---
+++ src/TuttiFrutti/ISie/Number/In/Go.cs
@@ -0,0 +1,78 @@
+﻿using System.Numerics;
+
+namespace ISie.Number.In;
+/// <summary>
+/// IMPORTANT: no extension for INumber since 1) `char` will get it, 2) generic parameter can't be specified implicitly
+/// </summary>
+public static partial class Integral_Signed
+{
+    public static string In<T>(this sbyte seed, IInclination? _ = null) where T : IScript, ISupported => Hub<T>.In(seed);
+    public static string In<T>(this short seed, IInclination? _ = null) where T : IScript, ISupported => Hub<T>.In(seed);
+    public static string In<T>(this int seed, IInclination? _ = null) where T : IScript, ISupported => Hub<T>.In(seed);
+    public static string In<T>(this long seed, IInclination? _ = null) where T : IScript, ISupported => Hub<T>.In(seed);
+    public static string In<T>(this nint seed, IInclination? _ = null) where T : IScript, ISupported => Hub<T>.In(seed);
+
+}
+
+public static partial class Integral_Signed
+{
+    public static string In<T>(this sbyte? seed, IInclination? _ = null) where T : IScript, ISupported
+        => ((sbyte)seed.NoNull()).In<T>();
+    public static string In<T>(this short? seed, IInclination? _ = null) where T : IScript, ISupported
+        => ((short)seed.NoNull()).In<T>();
+    public static string In<T>(this int? seed, IInclination? _ = null) where T : IScript, ISupported
+        => ((int)seed.NoNull()).In<T>();
+    public static string In<T>(this long? seed, IInclination? _ = null) where T : IScript, ISupported
+        => ((long)seed.NoNull()).In<T>();
+    public static string In<T>(this nint? seed, IInclination? _ = null) where T : IScript, ISupported
+        => ((nint)seed.NoNull()).In<T>();
+}
+
+public static partial class Integral_Unsigned
+{
+    public static string In<T>(this byte seed, IInclination? _ = null) where T : IScript, ISupported => Hub<T>.In(seed);
+    public static string In<T>(this ushort seed, IInclination? _ = null) where T : IScript, ISupported => Hub<T>.In(seed);
+    public static string In<T>(this uint seed, IInclination? _ = null) where T : IScript, ISupported => Hub<T>.In(seed);
+    public static string In<T>(this ulong seed, IInclination? _ = null) where T : IScript, ISupported => Hub<T>.In(seed);
+    public static string In<T>(this nuint seed, IInclination? _ = null) where T : IScript, ISupported => Hub<T>.In(seed);
+}
+public static partial class Integral_Unsigned
+{
+    public static string In<T>(this byte? seed, IInclination? _ = null) where T : IScript, ISupported
+        => ((byte)seed.NoNull()).In<T>();
+    public static string In<T>(this ushort? seed, IInclination? _ = null) where T : IScript, ISupported
+        => ((ushort)seed.NoNull()).In<T>();
+    public static string In<T>(this uint? seed, IInclination? _ = null) where T : IScript, ISupported
+        => ((uint)seed.NoNull()).In<T>();
+    public static string In<T>(this ulong? seed, IInclination? _ = null) where T : IScript, ISupported
+        => ((ulong)seed.NoNull()).In<T>();
+    public static string In<T>(this nuint? seed, IInclination? _ = null) where T : IScript, ISupported
+        => ((nuint)seed.NoNull()).In<T>();
+}
+
+public static class FloatingPoint
+{
+    public static string In<T>(this float seed, IInclination? _ = null) where T : IScript, ISupported
+        => Hub<T>.In(seed);
+
+    public static string In<T>(this double seed, IInclination? _ = null) where T : IScript, ISupported
+    => Hub<T>.In(seed);
+
+    public static string In<T>(this decimal seed, IInclination? _ = null) where T : IScript, ISupported
+        => Hub<T>.In(seed);
+    public static string In<T>(this float? seed, IInclination? _ = null) where T : IScript, ISupported
+        => ((float)seed.NoNull()).In<T>();
+
+    public static string In<T>(this double? seed, IInclination? _ = null) where T : IScript, ISupported
+        => ((double)seed.NoNull()).In<T>();
+
+    public static string In<T>(this decimal? seed, IInclination? inclination = null) where T : IScript, ISupported
+        => ((decimal)seed.NoNull()).In<T>();
+}
+
+static class Hub<T>
+{
+    internal static string In<N>(N seed) where N : INumber<N> => NotImplemented.Throw("under construction");
+}
+
+public interface IInclination;
---
+++ src/TuttiFrutti/ISie_Eval/BuiltinTypes/🔢Number/✨Demo_In🔢.cs
@@ -0,0 +1,42 @@
+﻿using AbcLingua.Tongues;
+using ISie.Number.In;
+namespace ISie_Eval.BuiltinTypes.Number;
+public class Demo_In : Setup.Arrange
+{
+    [SyntaxDemo]
+    public void Basic_Wording() {
+        uint year = 2025;
+        _ = $"This project began in the year: {year.In<English>()}";
+        _ = DateTime.Today.Day.In<Latin>();
+        _ = ulong.MaxValue.In<English>();
+
+        short angle = 45;
+        _ = angle.In<Greek>();
+
+        var uno = 1.In<Current>();
+        var minusOne = (1 - 2).In<Current>();
+
+        // ToDo: add nullable variants
+    }
+
+    [SyntaxDemo]
+    public void Float() {
+        decimal sum = 724.50M;
+        var report = $"total: {sum.In<Current>()}";
+
+        float? calc = -0.050_801F;
+        _ = calc.In<English>();
+        _ = double.Pi.In<French>(); // challenge - how many numbers at long fraction?
+    }
+
+    [SyntaxDemo]
+    public void RomanNumbers() {
+        // no zero and negatives
+    }
+
+    public void MiscFormats() {
+
+    }
+
+    // .FRACTION_STYLE - words or ?; // .Words;
+}
