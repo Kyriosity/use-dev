8323927b627d75413b93eb5a9e0a820c58d5225b
Kiryl Drougakov <kiryl_d@gmx.net>
2025-05-21 18:44:16 +0200

    ❗CodeWizard: `And` with `Or` as Next for ready blocks (to apply)

--- src/TuttiFrutti/WizConstr/🔗Blocks/Fuse/Bool/basal/rightVoid.cs
+++ src/TuttiFrutti/WizConstr/🔗Blocks/Fuse/Bool/basal/rightVoid.cs
@@ -7,4 +7,12 @@ public abstract class rightVoid<T> : Fuse.basal.rightVoid<T, bool>
     protected virtual Lnk_Fruit<T, bool, IDisjunct> Or(Func<T, bool> xform) => Next<IDisjunct>(xform);
     protected virtual Lnk_Fruit<T, bool, IConjunct> And(Func<T, bool> xform) => Next<IConjunct>(xform);
     //protected virtual Lnk_Fruit<T, bool, IExclude> Xor(Func<T, bool> xform) => Next<IExclude>(xform);
+
+    // ToDo: apply and test And and Or for supplied blocks (pulp and fruit) ⬇️
+
+    protected virtual TBk And<TBk>() where TBk : Pulp<T>, INoLnkLeft, new() =>
+        new() { LnkLeft = Unpack<IConjunct>() };
+
+    protected virtual TBk Or<TBk>() where TBk : Pulp<T>, INoLnkLeft, new() =>
+        new() { LnkLeft = Unpack<IDisjunct>() };
 }
