8a915be57463061ad24b88473494846fdb1d3526
Kiryl Drougakov <kiryl_d@gmx.net>
2025-06-29 17:20:00 +0200

    🚧placeholders for async (trash-version)

--- src/TuttiFrutti/AbcAsync_Eval/Demo.cs
+++ src/TuttiFrutti/AbcAsync_Eval/Demo.cs
@@ -4,6 +4,18 @@ public class Demo : Setup.Arrange
 {
     [Test]
     public void Test1() {
-        Assert.Pass();
+        var a = 5;
+        var c = -11;
+
+        (a, c) = (c, a);
+        // Method1();
+    }
+
+    async void Method1() {
+        var task = RunLongForAsync(5);
+
+        var status = task.Status;
+
+        int result = await task;
     }
 }
--- src/TuttiFrutti/AbcAsync_Eval/⚙️setup/Arrange.cs
+++ src/TuttiFrutti/AbcAsync_Eval/⚙️setup/Arrange.cs
@@ -1,4 +1,8 @@
 ﻿namespace AbcAsync_Eval.Setup;
 public abstract class Arrange
 {
+    protected async Task<int> RunLongForAsync(ushort seconds) {
+        await Task.Delay(1_000 * seconds);
+        return seconds; // ToDo: cancellation and elapsed
+    }
 }
