f5324912ef41b9ff878e3b1cd0d7885400b94bc0
Kiryl Drougakov <kiryl_d@gmx.net>
2025-07-09 22:55:49 +0200

    gRPC: configure

--- src/TuttiFrutti/Primus/Primus.Server/Program.cs
+++ src/TuttiFrutti/Primus/Primus.Server/Program.cs
@@ -5,11 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.MapStaticAssets();
+//app.MapGrpcService<>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
