031dc741414d4f678bdf24a23386fa3f9607d49d
Kiryl Drougakov <kiryl_d@gmx.net>
2025-07-28 21:57:59 +0200

    ⚛️TypeScript sandbox: triffles

--- src/TuttiFrutti/GoodTS/Tricks/XformTypes.ts
+++ src/TuttiFrutti/GoodTS/Tricks/XformTypes.ts
@@ -9,3 +9,19 @@ type ReadOnlyProps<T> = {
 
 
 type ReadonlySimple = ReadOnlyProps<Simple>
+
+
+// ENUM LIKE
+const ODirection = {
+    Up: "Up",
+    Down: "Down",
+    Left: "Left",
+    Right: "Right",
+} as const;
+
+
+type Direction = typeof ODirection[keyof typeof ODirection];
+
+function run(dir: Direction) { }
+
+run(ODirection.Right);
