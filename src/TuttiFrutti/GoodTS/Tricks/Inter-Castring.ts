b94085c7fd0fe34ccc857f5423d9d4472b8cd8eb
Kiryl Drougakov <kiryl_d@gmx.net>
2025-07-30 22:08:19 +0200

    ⚛️Typescript sandboxing

---
+++ src/TuttiFrutti/GoodTS/Tricks/Inter-Castring.ts
@@ -0,0 +1,20 @@
+interface ISubject  {
+    id: number
+    title: string
+    feat: boolean
+}
+
+type Subject = {
+    id: number
+    title: string
+
+}
+
+
+let ifaced: ISubject = { id: 1, title: "primus", feat: true }
+const subjTwo: Subject = ifaced;
+
+const free = { id: -5, title: "freefrom", feat: false }
+ifaced = free
+
+var anyone: ISubject | Subject
--- src/TuttiFrutti/GoodTS/Tricks/TypeFusion.ts
+++ src/TuttiFrutti/GoodTS/Tricks/TypeFusion.ts
@@ -3,9 +3,25 @@ type GeomShape =
    | { form: "rect"; width: number, height: number }
     | { form: "square"; side: number }
 
+type Unknown = { state: "Unknown" }
+type Loading = { state: "Loading", start: Date  }
+
+type Success = {
+    state: "Succes"
+    data: string
+}
+type Failed = {
+    state: "Failed"
+    code: number
+}
+
+type OpState = Unknown | Success | Loading | Error
+
 // ---- UTILITY TYPES ---
 // Partial
 // Omit
 // Readonly = here prebuilt
 // Pick
 // Awaited
+
+
