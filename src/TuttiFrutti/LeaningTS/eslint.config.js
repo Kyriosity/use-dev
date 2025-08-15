1aeaee19b3c10ab6c29f7b19a1365c58b7fa9f2f
Kiryl Drougakov <kiryl_d@gmx.net>
2025-08-15 13:09:17 +0200

    eslint rules: off+

--- src/TuttiFrutti/LeaningTS/eslint.config.js
+++ src/TuttiFrutti/LeaningTS/eslint.config.js
@@ -1,7 +1,14 @@
 module.exports = [
     {
         rules: {
-            // Add rules here.
+            '@typescript-eslint/no-unused-vars': 'off',
+            "no-unused-vars": ["off", {
+                "vars": "all",
+                "args": "after-used",
+                "caughtErrors": "all",
+                "ignoreRestSiblings": false,
+                "reportUsedIgnorePattern": false
+            }]
         }
     }
 ];
